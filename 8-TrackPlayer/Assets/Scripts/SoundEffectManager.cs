97801e9d0834b15ed2da2f70abc8ed2756432afa
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-09-15 22:18:59 -0400

    Sound effects scripts

---
+++ 8-TrackPlayer/Assets/Scripts/SoundEffectManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEffectManager : MonoBehaviour {
+    public AudioClip[] soundEffects;
+
+
+
+    AudioSource aSource;
+
+    public const int RESET = 0;
+    public const int PLAY = 1;
+    public const int PAUSE = 2;
+
+    void Awake()
+    {
+        aSource = GetComponent<AudioSource>();
+
+    }
+
+    public void playSound(int soundLocation)
+    {
+        aSource.Stop();
+        aSource.clip = soundEffects[soundLocation];
+        aSource.Play();
+    }
+}
