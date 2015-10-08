571b827c3a7ca9377761ab4be340593805732999
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-10-08 00:54:26 -0400

    GUI implemented
    Need sounds

---
+++ PolygraphTest/Assets/Scripts/HandleHeartRate.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HandleHeartRate : MonoBehaviour {
+    public Text heartRateText;
+    public Slider heartRateSlider;
+    public Slider heartRateVolume;
+    public float currentHeartRate = 60;
+
+    private AudioSource aSource;
+
+    void Update()
+    {
+        heartRateText.text = "" + (int)currentHeartRate;
+    }
+
+    public void setVolume(float volume)
+    {
+        aSource.volume = volume;
+    }
+
+    public void setHeartRate(float heartRate)
+    {
+        this.currentHeartRate = heartRate;
+    }
+
+    public void setVolumeSlider(float value)
+    {
+        this.heartRateVolume.value = value;
+    }
+
+    public void setRateSlider(float value)
+    {
+        this.heartRateSlider.value = value;
+    }
+
+    void Awake()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
+}
---
+++ PolygraphTest/Assets/Scripts/HandleNotification.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HandleNotification : MonoBehaviour {
+    public Slider slider;
+    public AudioClip clip;
+    public AudioSource aSource;
+
+    void Update()
+    {
+        
+    }
+
+    public void setIntensity(float intensity)
+    {
+        slider.value = intensity;
+    }
+
+    public void playSound()
+    {
+        aSource.Stop();
+        aSource.clip = clip;
+        aSource.Play();
+    }
+	
+}
